6207bb0d90b5ecb680b12cb427abc00aa72aaf5d
mdelapintiere <mdelapintiere@wolf-sa.fr>
2025-12-09 16:44:17 +0100

    test 1

--- src/regulstock/pipelines/correspondance/pipeline.py
+++ src/regulstock/pipelines/correspondance/pipeline.py
@@ -2,9 +2,10 @@ from kedro.pipeline import Pipeline, node, pipeline
 from .nodes import (
     standardize_m3,
     standardize_reflex,
-    add_reflex_category,
-    expand_reflex_to_m3_candidates,
-    join_with_m3_real,
+    map_reflex_category,
+    map_m3_category,
+    build_reflex_m3_wide_with_lotless,
+    compute_m3_reliquat
 )
 
 def create_pipeline(**kwargs) -> Pipeline:
@@ -14,24 +15,34 @@ def create_pipeline(**kwargs) -> Pipeline:
             node(standardize_reflex, "reflex_stock_parquet", "reflex_std", name="standardize_reflex"),
 
             node(
-                add_reflex_category,
+                map_reflex_category,
                 inputs=dict(reflex_df="reflex_std", mapping="params:reflex_mapping_rules"),
-                outputs="reflex_categorized",
-                name="add_reflex_category",
+                outputs="reflex_cat",
+                name="map_reflex_category",
+            ),
+            node(
+                map_m3_category,
+                inputs=dict(m3_df="m3_std", rules="params:m3_mapping_rules"),
+                outputs="m3_cat",
+                name="map_m3_category",
             ),
 
             node(
-                expand_reflex_to_m3_candidates,
-                inputs=dict(reflex_df="reflex_categorized", rules="params:reflex_to_m3_rules"),
-                outputs="reflex_m3_candidates",
-                name="expand_reflex_to_m3_candidates",
+                build_reflex_m3_wide_with_lotless,
+                inputs=dict(
+                    reflex_cat="reflex_cat",
+                    m3_cat="m3_cat",
+                    depots="params:m3_depots_columns",
+                ),
+                outputs="reflex_m3_wide",
+                name="build_reflex_m3_wide",
             ),
-
+            
             node(
-                join_with_m3_real,
-                inputs=dict(reflex_candidates="reflex_m3_candidates", m3_df="m3_std"),
-                outputs="reflex_m3_correspondence",
-                name="join_candidates_with_m3",
+                compute_m3_reliquat,
+                inputs=dict(m3_cat="m3_cat", reflex_cat="reflex_cat"),
+                outputs="m3_reliquat",
+                name="compute_m3_reliquat_node",
             ),
         ]
     )
--- src/regulstock/pipelines/extraction/pipeline.py
+++ src/regulstock/pipelines/extraction/pipeline.py
@@ -7,10 +7,10 @@ def create_pipeline(**kwargs) -> pipeline:
                 func=sql_chunks_to_parquet,
                 inputs=dict(
                     chunks="m3_stock_dataset",
-                    parquet_path="params:m3_parquet_path",
+                    parquet_path="params:m3_stock_parquet_path",
                     compression="params:parquet_compression",
                 ),
-                outputs="m3_parquet_written_path",
+                outputs="m3_stock_parquet_written_path",
                 name="m3_sql_chunks_to_parquet",
             ),
             node(
@@ -24,5 +24,17 @@ def create_pipeline(**kwargs) -> pipeline:
                 ),
                 outputs="reflex_parquet_written_path",
                 name="reflex_sql_chunks_to_parquet",
+            ),
+            node(
+                func=sql_chunks_to_parquet,
+                inputs=dict(
+                    chunks="m3_articles_dataset",
+                    parquet_path="params:m3_articles_parquet_path",
+                    compression="params:parquet_compression",
+                    # Kedro accepte les inputs non utilisés si on les met dans un dict,
+                    # mais la fonction doit les recevoir -> on wrappe via un lambda :
+                ),
+                outputs="m3_articles_parquet_written_path",
+                name="m3_articles_sql_chunks_to_parquet",
             ),
     ])
